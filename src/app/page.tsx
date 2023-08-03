43f77981244388163ecd886a2c8e3d4200ce96f1
Pedro Nest <pedronestjp@gmail.com>
2023-08-03 09:58:06 -0300

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,16 +14,24 @@ export default function Home() {
             height={200}
           />
           <div className={styles.navLinks}>
-            <span className={styles.navLink}>Serviços</span>
-            <span className={styles.navLink}>O Studio</span>
-            <span className={styles.navLink}>Contato</span>
-            <span className={styles.navLink}>Trajetória</span>
+            <span className={styles.navLink}>
+              <a href="#services">Serviços</a>
+            </span>
+            <span className={styles.navLink}>
+              <a href="#aboutStudio">O Studio</a>
+            </span>
+            <span className={styles.navLink}>
+              <a href="#contact">Contato</a>
+            </span>
+            <span className={styles.navLink}>
+              <a href="#trajectory">Trajetória</a>
+            </span>
           </div>
         </div>
         <div className={styles.content}>
           <div className={styles.contentText}>
             <h1>PRISCILA PONTES</h1>
-            <h3>Olá! Sou Priscila Pontes, apaixonada por unhas e trabalho nessa área desde 2019. Meu objetivo é oferecer serviços de qualidade e cuidado personalizado para cada cliente. Busco aprimorar minhas habilidades constantemente, e já concluí 4 cursos presenciais e 3 online, incluindo o renomado curso &quot;Instrutor de Sucesso &quot; ministrado por Katiane Brito. Estou sempre em busca de novas técnicas e tendências para garantir que minhas clientes tenham uma experiência única e resultados deslumbrantes. Conte comigo para cuidar das suas unhas e realçar sua beleza. Estou ansiosa para recebê-lo(a) em meu espaço e proporcionar um serviço excepcional!</h3>
+            <p>Olá! Sou Priscila Pontes, apaixonada por unhas e trabalho nessa área desde 2019. Meu objetivo é oferecer serviços de qualidade e cuidado personalizado para cada cliente. Busco aprimorar minhas habilidades constantemente, e já concluí 4 cursos presenciais e 3 online, incluindo o renomado curso &quot;Instrutor de Sucesso &quot; ministrado por Katiane Brito. Estou sempre em busca de novas técnicas e tendências para garantir que minhas clientes tenham uma experiência única e resultados deslumbrantes. Conte comigo para cuidar das suas unhas e realçar sua beleza. Estou ansiosa para recebê-lo(a) em meu espaço e proporcionar um serviço excepcional!</p>
           </div>
           <div className={styles.imgHeader}>
             <Image
@@ -31,7 +39,8 @@ export default function Home() {
               className={styles.imgHeader}
               alt='Priscila Pontes'
               width={775}
-              height={700}
+              height={775}
+
             />
           </div>
         </div>
