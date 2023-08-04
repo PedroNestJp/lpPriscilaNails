d0013552b923fe682bc941f151aaa322307237fc
Pedro Nest <pedronestjp@gmail.com>
2023-08-03 22:01:47 -0300

    up3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -29,8 +29,8 @@ export default function Home() {
           </div>
         </div>
         <div className={styles.content}>
-          <div className={styles.contentText}>
-            <h1>PRISCILA PONTES</h1>
+          <div className={styles.aboutMe}>
+            <h1 className={styles.contentTitle}>PRISCILA PONTES</h1>
             <p>Olá! Sou Priscila Pontes, apaixonada por unhas e trabalho nessa área desde 2019. Meu objetivo é oferecer serviços de qualidade e cuidado personalizado para cada cliente. Busco aprimorar minhas habilidades constantemente, e já concluí 4 cursos presenciais e 3 online, incluindo o renomado curso &quot;Instrutor de Sucesso &quot; ministrado por Katiane Brito. Estou sempre em busca de novas técnicas e tendências para garantir que minhas clientes tenham uma experiência única e resultados deslumbrantes. Conte comigo para cuidar das suas unhas e realçar sua beleza. Estou ansiosa para recebê-lo(a) em meu espaço e proporcionar um serviço excepcional!</p>
           </div>
           <div className={styles.imgHeader}>
